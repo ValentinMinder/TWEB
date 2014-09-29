eea34a31e74d3e2fe4c61893ec30858432f76eca
Iosis555 <pierre-alain.curty@heig-vd.ch>
2014-09-29 16:00:48 +0200

    heroku

---
+++ labo3/serverHTTP.js
@@ -0,0 +1,19 @@
+/**
+ * Imports
+ */
+var http = require('http');
+var url = require('url');
+
+/**
+ * Create a new HTTP server
+ */
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write('Ce cours me fout le cancer.');
+    res.end();
+});
+
+/**
+ * Launch server by make it listen to the port 8080
+ */
+server.listen(8080);
